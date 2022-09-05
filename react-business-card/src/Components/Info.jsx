41557ee142a8c8b0d87827be44713943e9480c43
timDeHof <this.is.tim.dehof@gmail.com>
2022-09-05 13:20:19 -0400

    added image of me to info component

--- react-business-card/src/Components/Info.jsx
+++ react-business-card/src/Components/Info.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import Me from "../assets/me-final.png";
+import { DiGithubBadge } from "react-icons/di";
+import { MdEmail } from "react-icons/md";
+import { BsLinkedin } from "react-icons/bs";
 const Info = () => {
   return (
-    <div>Info</div>
-  )
-}
+    <div className="info">
+      <img className="info--me" src={Me} />
+      <h1 className="info--fullName">Timothy DeHof</h1>
+      <p className="info--jobTitle">FrontEnd Developer</p>
+      <div className="info--cta">
+        <a className="info--cta_Btn email" href="mailto:ttdehof@gmail.com" target="_blank">
+          <MdEmail />
+          <span>
+          email
+          </span>
+        </a>
+        <a className="info--cta_Btn linkedin" href="https://www.linkedin.com/in/timothy-dehof/" target="_blank">
+          <BsLinkedin />
+          Linkedin
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Info
+export default Info;
